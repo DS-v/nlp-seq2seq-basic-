c86f643b2bd7b220a2a1c392f18867eb83780dc7
DS-v <59478620+DS-v@users.noreply.github.com>
2020-08-29 22:34:25 +0530

    Add files via upload

--- utils.py
+++ utils.py
@@ -54,8 +54,8 @@ def load_embeddings(embeddings_path):
     for line in open(embeddings_path, encoding='utf-8'):
         row = line.strip().split('\t')
         embeddings[row[0]] = np.array(row[1:], dtype=np.float32)
-    embeddings_dim = embeddings[list(embeddings)[0]].shape[0]
-    return embeddings,embeddings_dim
+    
+    return embeddings,100
 
     # Hint: you have already implemented a similar routine in the 3rd assignment.
     # Note that here you also need to know the dimension of the loaded embeddings.
